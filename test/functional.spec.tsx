eefb1fe9562ae1a7961eba2068405cf225dd7d8a
Rik Hoffbauer <Rikhoffbauer@gmail.com>
2019-06-02 00:14:19 +0200

    - log an error and deny permission if app isn't wrapped with an AbacProvider - test permissions directly set on user - test rendering with no roles and no permissions - test user with role that has no rules

--- test/functional.spec.tsx
+++ test/functional.spec.tsx
@@ -1,7 +1,7 @@
 import { mount } from "enzyme";
 import React from "react";
 
-import { AbacProvider, AllowedTo, Rules } from "../src";
+import { AbacProvider, AbacProviderProps, AllowedTo, Rules } from "../src";
 
 describe("Functional tests", () => {
     enum Role {
@@ -18,6 +18,7 @@ describe("Functional tests", () => {
     interface User {
         uuid: string;
         roles: Role[];
+        permissions: Permission[];
     }
 
     const rules: Rules<Role, Permission, User> = {
@@ -32,37 +33,63 @@ describe("Functional tests", () => {
                 user && data.uuid === user.uuid,
             [Permission.VIEW_HOMEPAGE]: true,
         },
+        [Permission.DELETE_USER]: (data: User, user?: User) =>
+            user && data.uuid === user.uuid,
     };
 
-    interface ProviderProps {
-        user: User;
-    }
-
-    const Provider: React.FC<ProviderProps> = ({ user, children }) => (
+    const Provider: React.FC<
+        Partial<AbacProviderProps<Role, Permission, User>>
+    > = ({ user, ...props }) => (
         <AbacProvider
-            roles={user.roles}
+            roles={user && user.roles}
+            permissions={user && user.permissions}
             user={user}
             rules={rules}
-            children={children}
+            {...props}
         />
     );
 
-    const admin: User = { uuid: "admin-uuid", roles: [Role.ADMIN] };
-    const user: User = { uuid: "user-uuid", roles: [Role.USER] };
+    const admin: User = {
+        uuid: "admin-uuid",
+        roles: [Role.ADMIN],
+        permissions: [],
+    };
+    const user: User = {
+        uuid: "user-uuid",
+        roles: [Role.USER],
+        permissions: [],
+    };
+    const userWithRbacPermissions: User = {
+        uuid: "user-with-rbac-permission",
+        roles: [],
+        permissions: [Permission.EDIT_USER],
+    };
+    const userWithAbacPermissions: User = {
+        uuid: "user-with-abac-permission",
+        roles: [],
+        permissions: [Permission.DELETE_USER],
+    };
 
     describe("<AllowedTo />", () => {
         describe("<AllowedTo perform={'PERMISSION'} />", () => {
-            it("Should throw an error if used without a provider", () => {
+            it("Should log an error if used without a provider and always render the no prop", () => {
                 const renderWithoutProvider = () =>
                     mount(
-                        <AllowedTo perform={Permission.EDIT_USER} data={user}>
+                        <AllowedTo
+                            perform={Permission.EDIT_USER}
+                            data={user}
+                            no={() => <div>No</div>}
+                        >
                             <div>yes</div>
                         </AllowedTo>,
                     );
 
-                expect(renderWithoutProvider).toThrow(
-                    `Wrap your app with an AbacProvider.`,
-                );
+                spyOn(console, "error");
+
+                const wrapper = renderWithoutProvider();
+
+                expect(wrapper.text()).toBe("No");
+                expect(console.error).toHaveBeenCalled();
             });
 
             it("Should render children if required rbac rules/permissions are met", () => {
@@ -75,7 +102,7 @@ describe("Functional tests", () => {
                     </Provider>,
                 );
 
-                expect(wrapper.find("div").text()).toBe("yes");
+                expect(wrapper.text()).toBe("yes");
             });
 
             it("Should not render children if required rbac rules/permissions are not met", () => {
@@ -90,7 +117,7 @@ describe("Functional tests", () => {
                     </Provider>,
                 );
 
-                expect(wrapper.find("div").text()).toBe("no");
+                expect(wrapper.text()).toBe("no");
             });
 
             it("Should render children if required abac rules/permissions are met", () => {
@@ -102,7 +129,7 @@ describe("Functional tests", () => {
                     </Provider>,
                 );
 
-                expect(wrapper.find("div").text()).toBe("yes");
+                expect(wrapper.text()).toBe("yes");
             });
 
             it("Should not render children if required abac rules/permissions are not met", () => {
@@ -118,7 +145,7 @@ describe("Functional tests", () => {
                     </Provider>,
                 );
 
-                expect(wrapper.find("div").text()).toBe("no");
+                expect(wrapper.text()).toBe("no");
             });
 
             it("Should render children if no rules/permissions are required", () => {
@@ -130,7 +157,7 @@ describe("Functional tests", () => {
                     </Provider>,
                 );
 
-                expect(wrapper.find("div").text()).toBe("yes");
+                expect(wrapper.text()).toBe("yes");
             });
 
             it("Should render nothing if no yes/no/children prop is required", () => {
@@ -143,6 +170,86 @@ describe("Functional tests", () => {
                 );
 
                 expect(wrapper.find("div").length).toBe(0);
+            });
+
+            it("Should render if the user has the required permission directly defined on it and no rule for it is defined", () => {
+                const wrapper = mount(
+                    <Provider user={userWithRbacPermissions}>
+                        <AllowedTo
+                            data={userWithRbacPermissions}
+                            perform={Permission.EDIT_USER}
+                        >
+                            <div>yes</div>
+                        </AllowedTo>
+                    </Provider>,
+                );
+
+                expect(wrapper.text()).toBe("yes");
+            });
+
+            it("Should render if the user has the required permission directly defined on it and its abac rule returns true", () => {
+                const wrapper = mount(
+                    <Provider user={userWithAbacPermissions}>
+                        <AllowedTo
+                            data={userWithAbacPermissions}
+                            perform={Permission.DELETE_USER}
+                        >
+                            <div>yes</div>
+                        </AllowedTo>
+                    </Provider>,
+                );
+
+                expect(wrapper.text()).toBe("yes");
+            });
+
+            it("Should not render if the user has the required permission directly defined on it and its abac rule returns false", () => {
+                const wrapper = mount(
+                    <Provider user={userWithAbacPermissions}>
+                        <AllowedTo
+                            data={user}
+                            perform={Permission.DELETE_USER}
+                            no={() => <div>no</div>}
+                        >
+                            <div>yes</div>
+                        </AllowedTo>
+                    </Provider>,
+                );
+
+                expect(wrapper.text()).toBe("no");
+            });
+
+            it("Should render as if no permission if there is no roles or permissions provided", () => {
+                const wrapper = mount(
+                    <Provider
+                        user={admin}
+                        roles={undefined}
+                        permissions={undefined}
+                    >
+                        <AllowedTo
+                            perform={Permission.VIEW_HOMEPAGE}
+                            no={() => <div>no</div>}
+                        >
+                            <div>yes</div>
+                        </AllowedTo>
+                    </Provider>,
+                );
+
+                expect(wrapper.text()).toBe("no");
+            });
+
+            it("Should render as if no permission if user has role that has no rules", () => {
+                const wrapper = mount(
+                    <Provider user={admin} rules={{}}>
+                        <AllowedTo
+                            perform={Permission.VIEW_HOMEPAGE}
+                            no={() => <div>no</div>}
+                        >
+                            <div>yes</div>
+                        </AllowedTo>
+                    </Provider>,
+                );
+
+                expect(wrapper.text()).toBe("no");
             });
         });
     });
